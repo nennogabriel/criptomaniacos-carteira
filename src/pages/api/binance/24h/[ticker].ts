3772a0e9a450cd0a48e43b07c89c3a84b55159ad
Pedro Moreno <nennogabriel@gmail.com>
2021-11-28 22:50:19 -0300

    fix: internal request not allowed

--- src/pages/api/binance/24h/[ticker].ts
+++ src/pages/api/binance/24h/[ticker].ts
@@ -1,16 +1,42 @@
 import axios from "axios";
 import type { NextApiRequest, NextApiResponse } from "next";
 
-const getChangesUrlApi = `${process.env.SELF_HOST}/api/binance/get24hs`;
+interface Change24Props {
+  [k: string]: string;
+}
+
+var changes: Change24Props = {};
+
+const changesUrlApi = "https://api.binance.com/api/v3/ticker/24hr";
+
+const changeUpdateInterval = 30000;
+var lastChangeDataUpdate = 0;
+
+async function updateChanges() {
+  if (new Date().getTime() < lastChangeDataUpdate + changeUpdateInterval)
+    return;
+  const responseChanges = await axios.get(changesUrlApi);
+
+  changes = Object.fromEntries(
+    responseChanges.data.map((item) => [
+      item.symbol.toUpperCase(),
+      item.priceChangePercent,
+    ])
+  );
+
+  console.log("changes cache updated.");
+
+  lastChangeDataUpdate = new Date().getTime();
+}
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   let ticker = req.query.ticker as string;
   ticker = ticker.toUpperCase();
 
-  axios.get(getChangesUrlApi).then((response) => {
-    const change = response.data[ticker];
-    if (change) {
-      return res.status(200).json(change);
+  updateChanges().then(() => {
+    const tickerChange = changes[ticker];
+    if (tickerChange) {
+      return res.status(200).json(tickerChange);
     } else {
       return res.status(404).json("ticker not found");
     }
--- src/pages/api/binance/price/[ticker].ts
+++ src/pages/api/binance/price/[ticker].ts
@@ -1,16 +1,38 @@
 import axios from "axios";
 import type { NextApiRequest, NextApiResponse } from "next";
 
-const getPricesUrlApi = `${process.env.SELF_HOST}/api/binance/getPrices`;
+interface PriceProps {
+  [k: string]: string;
+}
+
+var prices: PriceProps = {};
+
+const pricesUrlApi = "https://api.binance.com/api/v3/ticker/price";
+
+const priceUpdateInterval = 30000;
+var lastPriceDataUpdate = 0;
+
+async function updatePrices() {
+  if (new Date().getTime() < lastPriceDataUpdate + priceUpdateInterval) return;
+  const responsePrices = await axios.get(pricesUrlApi);
+
+  prices = Object.fromEntries(
+    responsePrices.data.map((item) => [item.symbol.toUpperCase(), item.price])
+  );
+
+  console.log("prices cache updated.");
+
+  lastPriceDataUpdate = new Date().getTime();
+}
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   let ticker = req.query.ticker as string;
   ticker = ticker.toUpperCase();
 
-  axios.get(getPricesUrlApi).then((response) => {
-    const price = response.data[ticker];
-    if (price) {
-      return res.status(200).json(price);
+  updatePrices().then(() => {
+    const tickerPrice = prices[ticker];
+    if (tickerPrice) {
+      return res.status(200).json(tickerPrice);
     } else {
       return res.status(404).json("ticker not found");
     }
