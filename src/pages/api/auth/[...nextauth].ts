d68eccb3ec6978c107445a8c0f8aae2a2d790543
Pedro Moreno <nennogabriel@gmail.com>
2022-03-08 12:10:13 -0300

    test: login email github

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -7,7 +7,7 @@ import { query as q } from "faunadb";
 import { fauna } from "../../../services/fauna";
 
 type UserProps = {
-  status: string;
+  status?: string;
 };
 
 export default NextAuth({
@@ -22,58 +22,56 @@ export default NextAuth({
       clientId: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
     }),
-    // GoogleProvider({
-    //   clientId: process.env.GOOGLE_CLIENT_ID,
-    //   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    // }),
-    // FacebookProvider({
-    //   clientId: process.env.FACEBOOK_CLIENT_ID,
-    //   clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
-    // }),
   ],
   callbacks: {
-    async session({ session, user, token }) {
-      try {
-        const faunaUserData = await fauna.query<UserProps>(
-          q.Select(
-            "data",
-            q.Get(
-              q.Match(q.Index("user_by_email"), q.Casefold(session.user.email))
-            )
-          )
-        );
-        session.status = faunaUserData.status;
-      } catch (err) {
-        console.log("Error when tried to get user status");
-      }
+    // async session({ session, user, token }) {
+    //   try {
+    //     const faunaUserData = await fauna.query<UserProps>(
+    //       q.Select(
+    //         "data",
+    //         q.Get(
+    //           q.Match(q.Index("user_by_email"), q.Casefold(session.user.email))
+    //         )
+    //       )
+    //     );
+    //     session.status = faunaUserData.status || 0;
+    //   } catch (err) {
+    //     console.log("Error when tried to get user status");
+    //   }
 
-      return session;
-    },
+    //   return session;
+    // },
 
     async signIn({ user, account, profile, email, credentials }) {
-      try {
-        await fauna.query(
-          q.If(
-            q.Not(
-              q.Exists(
-                q.Match(q.Index("user_by_email"), q.Casefold(user.email))
-              )
-            ),
-
-            q.Create(q.Collection("users"), {
-              data: {
-                email: user.email,
-                status: 0,
-              },
-            }),
-
-            q.Get(q.Match(q.Index("user_by_email"), q.Casefold(user.email)))
-          )
-        );
+      const emails = ["nennogabriel@gmail.com", "bruno@criptomaniacos.io"];
+      if (emails.some((email) => email === user.email)) {
         return true;
-      } catch (err) {
-        return false;
       }
+
+      return false;
+      // try {
+      //   await fauna.query(
+      //     q.If(
+      //       q.Not(
+      //         q.Exists(
+      //           q.Match(q.Index("user_by_email"), q.Casefold(user.email))
+      //         )
+      //       ),
+
+      //       q.Create(q.Collection("users"), {
+      //         data: {
+      //           email: user.email,
+      //           status: 0,
+      //         },
+      //       }),
+
+      //       q.Get(q.Match(q.Index("user_by_email"), q.Casefold(user.email)))
+      //     )
+      //   );
+      //   return true;
+      // } catch (err) {
+      //   return false;
+      // }
     },
   },
 });
