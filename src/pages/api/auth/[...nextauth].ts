b3e0fa4c36bf1864f8ffe6264c61e32c8f943c64
Pedro Moreno <nennogabriel@gmail.com>
2022-03-13 16:44:26 -0300

    test: jwt token

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -4,6 +4,7 @@ import CredentialsProvider from "next-auth/providers/credentials";
 
 import { query as q } from "faunadb";
 import { fauna } from "../../../services/fauna";
+import { telegram } from "../../../services/api";
 
 type UserProps = {
   role?: string;
@@ -28,17 +29,22 @@ export default NextAuth({
     CredentialsProvider({
       name: "Telegram",
       credentials: {},
-      async authorize(credentials, req) {
+      authorize: async (credentials, req) => {
         const data = req.query;
         if (!data) {
           return null;
         }
         const { id, username, first_name } = data;
 
+        const responseTelegramBot = await telegram.get(`/user_on_group/${id}`);
+
+        const { valid } = responseTelegramBot.data;
+
         const user = {
           id,
           name: `${first_name} (@${username})`,
           email: `${id}@web.telegram.org`,
+          role: valid ? 2 : 1,
         };
 
         if (user) {
@@ -54,7 +60,14 @@ export default NextAuth({
     }),
   ],
   callbacks: {
+    jwt: async ({ token, user }) => {
+      user?.role && (token.role = user.role);
+      console.log("token: ", token);
+      return token;
+    },
     session: async ({ session, user, token }) => {
+      console.log({ session, user, token });
+      token?.role && (session.role = token.role);
       try {
         const faunaUserData = await fauna.query<UserProps>(
           q.Select(
@@ -73,6 +86,9 @@ export default NextAuth({
     },
 
     signIn: async ({ user, account, profile, email, credentials }) => {
+      if (user?.role) {
+        return true;
+      }
       try {
         await fauna.query(
           q.If(
