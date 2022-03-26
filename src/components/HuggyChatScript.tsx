d96a1f8d1f7a963ca0043aa32d4a02e0bbb37370
Pedro Moreno <nennogabriel@gmail.com>
2022-03-26 11:13:48 -0300

    fix: chat widget

--- src/components/HuggyChatScript.tsx
+++ src/components/HuggyChatScript.tsx
@@ -4,7 +4,13 @@ const HuggyChatScript = () => {
   return (
     <div
       dangerouslySetInnerHTML={{
-        __html: `<script>(function (c,r,i,p,t,o) {p = c.createElement(r);p.async = 1;p.src = i;t = c.getElementsByTagName(r)[0];t.parentNode.insertBefore(p, t);})(document,"script","https://tools.criptomaniacos.dev/chat/widget.js",);</script>`,
+        __html: `<script>
+        !(function (b, t, c) {
+          ((c = t.createElement(b)).async = 1),
+            (c.src = 'https://s.cmania.co/site.js'),
+            (b = t.getElementsByTagName(b)[0]).parentNode.insertBefore(c, b);
+        })('script', document);
+      </script>`,
       }}
     ></div>
   );
