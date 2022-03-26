7e12251440ffa67e624c8dba33ff44e0c22a4dbc
Pedro Moreno <nennogabriel@gmail.com>
2022-03-26 11:17:13 -0300

    fix: chat widget

--- src/components/HuggyChatScript.tsx
+++ src/components/HuggyChatScript.tsx
@@ -7,7 +7,7 @@ const HuggyChatScript = () => {
         __html: `<script>
         !(function (b, t, c) {
           ((c = t.createElement(b)).async = 1),
-            (c.src = 'https://s.cmania.co/site.js'),
+            (c.src = 'https://s.cmania.co/membros.js'),
             (b = t.getElementsByTagName(b)[0]).parentNode.insertBefore(c, b);
         })('script', document);
       </script>`,
